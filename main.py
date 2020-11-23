d3f687f50619efd3c57665ffc4179f151a323fd5
Johan <3921204+johan-mattias@users.noreply.github.com>
2020-11-23 14:30:29 +0100

    added the automatic checking

--- main.py
+++ main.py
@@ -13,36 +13,65 @@ qwertyui -> reject (fail)
 
 '''
 
-password = "123qwe"
+''' password = "123qwe"
 
-basic.show_string(password)
+basic.show_string(password) '''
 
-#len is length
-if(len(password) < 8):
-    basic.show_string("Reject")
+passwords = ["1234qwer","12345678","qwertyui","123qwe",]
+passed_tests = [True, False, False, False]
 
-# list of letters
-# you're welcome
-letters = ['a', 'b', 'c', 'd', 
-           'e', 'f', 'g', 'h', 
-           'i', 'j', 'k', 'l', 
-           'm', 'n', 'o', 'p', 
-           'q', 'r', 's', 't', 
-           'u', 'v', 'w', 'x', 
-           'y', 'z']
+basic.show_string( run_tests(passwords, passed_tests))
 
-# list of numbers
-numbers = [1, 2, 3, 4, 5, 
-           6, 7, 8, 9, 0]
+def run_tests(passwords, passed_tests):
+    test_output = []
+    for password in passwords:
+        test_output.append(check_password(password))
+    
+    passed = True
+    for index in range(len(passed_tests)):
+        if passed_tests[index] != test_output[index]:
+            print("Fail test case #"+ index+ ": Result "+ test_output[index] + " expected"+ passed_tests[index])
+            passed = False
 
-letter_in_password = False
-number_in_password = False
+    return "pass" if passed == True else "fail"
+    
 
-for character in password:
-    if(character in letters):
-        letter_in_password = True
 
-if(letter_in_password == False):
-    basic.show_string("reject")
+def check_password(password: str):
+    #len is length
+    if(len(password) < 8):
+        return False
 
-basic.show_string("Accept")
+    # list of letters
+    # you're welcome
+    letters = ['a', 'b', 'c', 'd', 
+            'e', 'f', 'g', 'h', 
+            'i', 'j', 'k', 'l', 
+            'm', 'n', 'o', 'p', 
+            'q', 'r', 's', 't', 
+            'u', 'v', 'w', 'x', 
+            'y', 'z']
+
+    # list of numbers
+    numbers = [1, 2, 3, 4, 5, 
+            6, 7, 8, 9, 0]
+
+    letter_in_password = False
+    number_in_password = False
+
+    for character in password:
+        if(character in letters):
+            letter_in_password = True
+
+    for num_character in password:
+        num_char = int(num_character)
+        if(num_char in numbers):
+            number_in_password = True
+
+    if(letter_in_password == False):
+        return False
+
+    if(number_in_password == False):
+        return False
+
+    return True
--- main.ts
+++ main.ts
@@ -13,28 +13,64 @@ qwertyui -> reject (fail)
 
 
  */
-let password = "123qwe"
-basic.showString(password)
-// len is length
-if (password.length < 8) {
-    basic.showString("Reject")
+/**  password = "123qwe"
+
+basic.show_string(password) 
+ */
+let passwords = ["1234qwer", "12345678", "qwertyui", "123qwe"]
+let passed_tests = [true, false, false, false]
+basic.showString(run_tests(passwords, passed_tests))
+function run_tests(passwords: string[], passed_tests: boolean[]): string {
+    let test_output = []
+    for (let password of passwords) {
+        test_output.push(check_password(password))
+    }
+    let passed = true
+    for (let index = 0; index < passed_tests.length; index++) {
+        if (passed_tests[index] != test_output[index]) {
+            console.log("Fail test case #" + index + ": Result " + test_output[index] + " expected" + passed_tests[index])
+            passed = false
+        }
+        
+    }
+    return passed == true ? "pass" : "fail"
 }
 
-//  list of letters
-//  you're welcome
-let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-//  list of numbers
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-let letter_in_password = false
-let number_in_password = false
-for (let character of password) {
-    if (letters.indexOf(character) >= 0) {
-        letter_in_password = true
+function check_password(password: string): boolean {
+    let num_char: number;
+    // len is length
+    if (password.length < 8) {
+        return false
     }
     
-}
-if (letter_in_password == false) {
-    basic.showString("reject")
+    //  list of letters
+    //  you're welcome
+    let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    //  list of numbers
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    let letter_in_password = false
+    let number_in_password = false
+    for (let character of password) {
+        if (letters.indexOf(character) >= 0) {
+            letter_in_password = true
+        }
+        
+    }
+    for (let num_character of password) {
+        num_char = parseInt(num_character)
+        if (numbers.indexOf(num_char) >= 0) {
+            number_in_password = true
+        }
+        
+    }
+    if (letter_in_password == false) {
+        return false
+    }
+    
+    if (number_in_password == false) {
+        return false
+    }
+    
+    return true
 }
 
-basic.showString("Accept")
